2d69dd944edef32eeef3e6b95b171d3fd67d5195
cuongpham <cuongpham082@gmail.com>
2024-09-01 21:14:26 +0700

    scratch commit

---
+++ src/main/java/com/hc/cupcake/CupcakeApplication.java
@@ -0,0 +1,13 @@
+package com.hc.cupcake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CupcakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CupcakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hc/cupcake/CupcakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hc.cupcake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CupcakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
